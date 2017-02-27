f07a65e58a6da72054ef71d217a4785ab2ef1ac6
Matthew Moyer <matthewrmoyer@gmail.com>
2017-02-26 17:20:14 -0700

    looping over movie names in pug

--- routes.js
+++ routes.js
@@ -13,7 +13,7 @@ router.use(express.static(__dirname + '/public'))
 
 
 router.get('/', (req, res) => {
-  res.send('index.html')
+	res.send('index.html')
 })
 
 router.get('/new', (req, res) => {
@@ -28,20 +28,32 @@ router.get('/new', (req, res) => {
 router.get('/movies', (req, res) => {
 	const movieData = db.get('movies')
 	const movieDataStringified = JSON.stringify(movieData)
-	
+
 	const movieNames = db.get('movies').map('name')
+
+	var movieNamesArray = []
+	for (var i = 0; i < movieNames.size(); i++) {
+		var movieName = movieNames['__wrapped__']['movies'][i]['name']
+		console.log(movieName)
+		movieNamesArray.push(movieName)
+		console.log(movieNamesArray)
+
+	}
+
 	const movieNamesStringified = JSON.stringify(movieNames)
-	res.render('movies', 
-		{
-			title:"List Movies Crud", 
-			message: 'loop over movies here', 
-			movieList: movieNamesStringified
-		})
+	res.render('movies', {
+		title: "List Movies Crud",
+		message: 'loop over movies here',
+		movieList: movieNamesStringified,
+		movieNamesArray: movieNamesArray
+	})
 })
 
 router.get('/movies/:id', (req, res) => {
 	const id = parseInt(req.params.id)
-	const movie = db.get('movies').find({id: id})
+	const movie = db.get('movies').find({
+		id: id
+	})
 	res.send(movie)
 })
 
