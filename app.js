8fd0050dea51863836339d147a65a41ab50d813e
Matthew Moyer <matthewrmoyer@gmail.com>
2017-02-27 15:35:04 -0700

    herokuuu

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const routes = require('./routes.js')
 const bodyParser = require('body-parser')
 const path = require('path')
-
+var PORT = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 app.use('/', routes)
@@ -13,6 +15,6 @@ app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
 
 
-app.listen(8080, ()=> {
+app.listen(PORT, ()=> {
 	console.log('listening')
 })
